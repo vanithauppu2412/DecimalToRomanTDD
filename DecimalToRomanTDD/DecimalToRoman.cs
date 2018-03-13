8a2cc74bdf58cd7a8194cc4a133ff7279b4b5db6
Vanithauppu2412 <vanitha.srdev@gmail.com>
2018-03-13 11:16:37 -0700

    Add project files.

---
+++ DecimalToRomanTDD/DecimalToRoman.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DecimalToRomanTDD
+{
+    [TestClass]
+    public class DecimalToRomanTest
+    {
+        [TestMethod]
+        public void one()
+        {
+            Assert.AreEqual("I", DecimalToRoman.ConvertDecimalToRoman(1));
+        }
+    }
+}
---
+++ DecimalToRomanTDD/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DecimalToRomanTDD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DecimalToRomanTDD")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a62a1098-c5f0-4e39-a55e-425a4cababc1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
