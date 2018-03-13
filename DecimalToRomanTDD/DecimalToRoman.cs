c692be048e304a840eb1d49a75d9107c3350cb9b
Vanithauppu2412 <vanitha.srdev@gmail.com>
2018-03-13 11:40:07 -0700

    Green: Test passed for 5,6,7,8

--- DecimalToRomanTDD/DecimalToRoman.cs
+++ DecimalToRomanTDD/DecimalToRoman.cs
@@ -9,7 +9,18 @@ namespace DecimalToRomanTDD
         public static String ConvertDecimalToRoman(int number)
         {
             StringBuilder result = new StringBuilder();
-            for (int i = 0; i < number; i++)
+            int remaining = number;
+            if (remaining >= 5)
+            {
+                result.Append("V");
+                remaining -= 5;
+            }
+            if (remaining == 4)
+            {
+                result.Append("IV");
+                remaining -= 4;
+            }
+            for (int i = 0; i < remaining; i++)
             {
                 result.Append("I");
             }
@@ -20,10 +31,13 @@ namespace DecimalToRomanTDD
     public class DecimalToRomanTest
     {
         [TestMethod]
-        public void Five()
+        public void TestMethod()
         {
             Assert.AreEqual("V", DecimalToRoman.ConvertDecimalToRoman(5));
-           
+            Assert.AreEqual("VI", DecimalToRoman.ConvertDecimalToRoman(6));
+            Assert.AreEqual("VII", DecimalToRoman.ConvertDecimalToRoman(7));
+            Assert.AreEqual("VIII", DecimalToRoman.ConvertDecimalToRoman(8));
+
         }
     }
 }
